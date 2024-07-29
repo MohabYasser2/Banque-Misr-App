e4021186fd6060f7e52a70e0bcf9ddeee19e506b
Mohab Yasser <mohabhelp@gmail.com>
2024-07-29 19:55:35 +0300

    Magdy

--- app/src/main/java/com/groupd/banquemisrapp/MainActivity.kt
+++ app/src/main/java/com/groupd/banquemisrapp/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             BanqueMisrAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Magdy ",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     BanqueMisrAppTheme {
-        Greeting("Android")
+        Greeting("Magdy")
     }
 }
