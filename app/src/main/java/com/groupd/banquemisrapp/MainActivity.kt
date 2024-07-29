e626b16c711f2e76a3f2c561e243a3300ac8f7ec
Magdy372 <k.m334422@gmail.com>
2024-07-29 20:00:08 +0300

    ..

--- app/src/main/java/com/groupd/banquemisrapp/MainActivity.kt
+++ app/src/main/java/com/groupd/banquemisrapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -32,10 +33,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column {
+        Text(
+            text = "Hello Magdy!",
+            modifier = modifier
+        )
+        Text(
+            text = "Hello Mohab!",
+            modifier = modifier
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
