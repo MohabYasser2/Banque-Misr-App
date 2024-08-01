13728be93f1ebb99360a2f0e082c48337f8d2b69
Mohab Yasser <mohabhelp@gmail.com>
2024-08-02 01:19:23 +0300

    minor fix

--- app/src/main/java/com/groupd/banquemisrapp/ui/screens/main/MyCards.kt
+++ app/src/main/java/com/groupd/banquemisrapp/ui/screens/main/MyCards.kt
@@ -52,7 +52,7 @@ fun MyCardsScreen(navController: NavController, modifier: Modifier = Modifier) {
     Column(
         modifier = modifier
             .fillMaxSize()
-            .background(background)
+
 
     )
     {
